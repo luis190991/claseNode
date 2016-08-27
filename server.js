5a5f536e5107716f523deb75648f5b25610f96cb
luis190991 <luis190991@gmail.com>
2016-08-27 01:44:27 +0000

    Se agregan archivos de servidor.

---
+++ server.js
@@ -0,0 +1,9 @@
+//console.log("Hola Mundo!!!!");
+var http = require("http");
+var fs = require("fs");
+
+var html = fs.readFileSync("./index.html");
+http.createServer(function(req, res){
+                  res.write(html);
+                  res.end();
+                  }).listen(8080);
