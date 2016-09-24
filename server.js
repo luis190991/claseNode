111b87b75a772813f6234e9c19ac22dbc1489adf
luis190991 <luis190991@gmail.com>
2016-09-24 01:14:48 +0000

    Clase 23-09 se agrega parametros

--- server.js
+++ server.js
@@ -7,14 +7,28 @@ http.createServer(function(req, res){
     return ;
   }
 
+  var parametros = {};
+  var parametroArreglo = [];
+
+  if(req.url.indexOf("?") >0){
+    var urlData = req.url.split("?");
+    parametroArreglo = urlData[1].split("&");
+
+  var parametro = {};
+    for(parametro of parametroArreglo){
+      var paramData = parametro.split("=");
+      parametros[paramData[0]] = paramData[1];
+    }
+  }
+
  fs.readFile("./index.html", function(err, html){
    var htmlToString = html.toString();
    var variables = htmlToString.match(/[^\{\}]+(?=\})/g);
-   var nombre = "Luis";
-   var apellido = "Ramirez";
+
    for(variable of variables){
-     var valor = eval(variable)
-     htmlToString = htmlToString.replace(`{${variable}}`, valor);
+     //var valor = eval(variable)
+     htmlToString = htmlToString
+       .replace(`{${variable}}`, parametros[variable]);
    }
    res.writeHeader(200,{"Content-Type":"text/html"});
    res.write(htmlToString);
